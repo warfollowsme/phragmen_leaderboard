0f64e9fe3de9d469de59ce2b919c7f840beded43
root <root@ovh1.server.ryabina.io>
2020-06-18 12:15:08 +0000

    fix 'no such file or directory' error

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ const fs = require('fs')
 var app = express()
 
 app.get('/', function (req, res) {
-    var winners = JSON.parse(fs.readFileSync(`./results/winners20.json`, 'utf8'))
-    var result = JSON.stringify(winners);
-    res.send(result);
+    if (!fs.existsSync('./results/winners20.json')) {
+        res.send('no data has been created');
+    }
+    else{
+        var result = fs.readFileSync(`./results/winners20.json`, 'utf8');
+        res.send(result);
+    }
 })
 
-app.get('/50', function (req, res) {
-    var winners = JSON.parse(fs.readFileSync(`./results/winners50.json`, 'utf8'))
-    var result = JSON.stringify(winners);
-    res.send(result);
+app.get('/:num', function (req, res) {
+    if (!fs.existsSync(`./results/winners${req.params.num}.json`)) {
+        res.send('no data has been created');
+    }
+    else {
+        var result = fs.readFileSync(`./results/winners${req.params.num}.json`, 'utf8');
+        res.send(result);
+    }
 })
 
 startScanning();
@@ -23,7 +31,9 @@ app.listen(3000)
 async function startScanning() {
     await init()
     getLeaderboard(20)
+    getLeaderboard(25)
     getLeaderboard(50)
     setInterval(getLeaderboard, 180000, 20);
+    setInterval(getLeaderboard, 180000, 25);
     setInterval(getLeaderboard, 180000, 50);
 }
